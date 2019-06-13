69c1fd95c09a8affe90452ce9c998b20284225d1
= <=>
2019-06-12 23:45:48 -0300

    Realizar cadastro de uma nova postagem!

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import api from '../services/api';
 import './Feed.css'
 
 import more from '../assets/more.svg';
@@ -8,54 +8,45 @@ import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
 class Feed extends Component {
+    state = {
+        feed: [],
+    };
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+        this.setState({feed: response.data});
+    }
+
+
     render () {
         return (
-           <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Felipe Rodrigues</span>
-                            <span className="place">Parnamirim</span>
-                        </div>
+           <section id="post-list">              
+                { this.state.feed.map( post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
 
-                        <img src={more} alt="Mais"/>
-                    </header>
+                            <img src={more} alt="Mais"/>
+                        </header>
 
-                    <img src="http://localhost3333/files/template.jpg" alt=""/>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
-                        <strong>900 curtidas</strong>
-                        <p> SKASAKSAKSAKSAK</p>
-                    </footer>
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt=""/>
+                                <img src={comment} alt=""/>
+                                <img src={send} alt=""/>
+                            </div>
+                            <strong>{post.likes}</strong>
+                            <p> {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
                 </article>
-
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Felipe Rodrigues</span>
-                            <span className="place">Parnamirim</span>
-                        </div>
-
-                        <img src={more} alt="Mais"/>
-                    </header>
-
-                    <img src="http://localhost3333/files/template.jpg" alt=""/>
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
-                        <strong>900 curtidas</strong>
-                        <p> SKASAKSAKSAKSAK</p>
-                    </footer>
-                </article>
+                ))}
             </section>
         )
     }
